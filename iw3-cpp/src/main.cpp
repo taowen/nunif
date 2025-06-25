f55d0b4ce7fdf1480ca9c19ee0d4f4a0c47e3c1a
Tao Wen <taowen@gmail.com>
2025-06-25 08:32:51 +0800

    update

--- iw3-cpp/src/main.cpp
+++ iw3-cpp/src/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <format>  // C++20 特性
-#include <string_view>  // C++17 特性
+#include <format>
+#include <string_view> 
 
 int main() {
     constexpr std::string_view message = "Hello, World!";
-    
     std::cout << std::format("Message: {}\n", message);
     return 0;
 }
